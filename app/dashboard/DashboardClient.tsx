81efd0e7e5b6170ba7c0b391ac872822c4a6d52e
Jimmy <contact@inrcy.com>
2026-02-11 19:53:46 +0100

    0123

--- app/dashboard/DashboardClient.tsx
+++ app/dashboard/DashboardClient.tsx
@@ -2206,7 +2206,7 @@ const disconnectSiteWebGsc = useCallback(() => {
 </svg>
 </button>
 
-      <div className={styles.loopSub}>Toutes vos communications partent d'ici</div>
+      <div className={styles.loopSub}>Tout part d'ici</div>
       <div className={styles.loopActions}>
         <button
   className={`${styles.actionBtn} ${styles.connectBtn}`}
